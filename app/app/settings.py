25f05455e41aa118890f799a07c069b61a6933c7
imnotanalien <sokdenspace@outlook.com>
2023-07-04 01:40:17 +0700

    Update settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -123,10 +123,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/var/www/static/'),
+    os.path.join(BASE_DIR, 'static/'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/var/www/static/')
+STATIC_ROOT = "/var/www/cambodiaedu.org/static/"
+# STATIC_ROOT = os.path.join(BASE_DIR, '/var/www/static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

fae102919af7a8f66a5dd39db87e19d62b84ff6d
thisisdensok <sokdenspace@outlook.com>
2024-03-02 20:40:56 +0700

    Added CloudFront for invalidation remote

---
+++ cloudfront/invalidation.py
@@ -0,0 +1,30 @@
+# To ckeck AWS CLI connected with IAM user or not by "~/.aws/credentials" and "~/.aws/config"
+import boto3
+import time
+ 
+# Create CloudFront client
+cf = boto3.client("cloudfront")
+
+# This is the CloudFront Distribution ID.
+DISTRIBUTION_ID = ""
+
+# Create CloudFront invalidation.
+def create_invalidation():
+    res = cf.create_invalidation(
+        DistributionId=DISTRIBUTION_ID,
+        InvalidationBatch={
+            'Paths': {
+                'Quantity': 1,
+                'Items': [
+                    "/static/src/*",
+                ]
+            },
+            'CallerReference': str(time.time()).replace(".", "")
+        }
+    )
+    invalidation_id = res['Invalidation']['Id']
+    return invalidation_id
+
+# Create CloudFront Invalidation.
+id = create_invalidation()
+print("Invalidation created successfully with Id: " + id)

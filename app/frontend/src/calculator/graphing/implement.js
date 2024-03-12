1724c9585ba39e6083eaab817ad605f2fd118f22
thisisdensok <sokdenspace@outlook.com>
2024-03-13 00:43:49 +0700

    Update graphing calculator

--- app/frontend/src/calculator/graphing/implement.js
+++ app/frontend/src/calculator/graphing/implement.js
@@ -2,45 +2,45 @@
 
 //---- Constants:
 
-//Default values:
+// Default values:
 
-//Scale:
+// Scale:
 const Y_SCALE_DEFAULT = 1;
 const X_SCALE_DEFAULT = 1;
 
-//Offset:
+// Offset:
 const Y_OFFSET_DEFAULT = 0;
 const X_OFFSET_DEFAULT = 0;
 
-//Values at the edge of the canvas:
+// Values at the edge of the canvas:
 const Y_MAX = 10;
 const Y_MIN = -10;
 const X_MAX = 10;
 const X_MIN = -10;
 
-//Axis marks:
+// Axis marks:
 const TICK_WIDTH = 0.1;
 const TICK_DIGITS = 2;
 
-//Axis maninupulation
+// Axis maninupulation
 const OFFSET_CHANGE = 1;		//Change in position of the axes
 const SCALE_FACTOR = 2;			//How much the axis is multiplied or divided by
 
 //---- Global variables:
 
-//Scale:
+// Scale:
 let XScale = Y_SCALE_DEFAULT;
 let YScale = X_SCALE_DEFAULT;
 	
-//Offset:
+// Offset:
 let Yoffset = Y_OFFSET_DEFAULT;
 let Xoffset = X_OFFSET_DEFAULT;
 
-//HTML canvas:
+// HTML canvas:
 let canvas = document.getElementById( "canvas" );
 let context = canvas.getContext("2d");
 
-//Graphs:
+// Graphs:
 let graph1 = new graph();
 let graph2 = new graph();
 let graph3 = new graph();
